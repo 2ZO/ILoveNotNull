213d1a156ebca8492fb7a7cb1440306eca49e4b2
User <User@192.168.0.107>
2018-03-30 22:09:34 +0900

    Revert "선생님 추가관련 파일 업로드"
    This reverts commit 3c7855b354d5f8689f07a2b288d8170f1eed2de8.

--- BbacksYoga/src/org/kosta/controller/second/Create_TeacherController.java
+++ BbacksYoga/src/org/kosta/controller/second/Create_TeacherController.java
@@ -4,19 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.kosta.controller.Controller;
-import org.kosta.model.DAO.TeacherDAO;
 
 public class Create_TeacherController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String name=request.getParameter("teacherName");
-		String nick=request.getParameter("teacherNick");
-		String profile=request.getParameter("teacherProfile");
-		String imgURL=request.getParameter("teacherImgURL");
-		TeacherDAO.getInstance().AddTeacher(name,nick,profile,imgURL);
-		
-		return "redirect:admin_ver2/admin.jsp";
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
