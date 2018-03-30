114916b1791445e400cc09946c58ccba9195b10e
User <User@192.168.0.107>
2018-03-30 22:04:12 +0900

    Revert "Revert "Revert "Revert "선생님 추가관련 파일 업로드""""
    This reverts commit 7e152743d41e65c31a818ee540f957e3fe9529e9.

--- BbacksYoga/src/org/kosta/controller/second/Create_TeacherController.java
+++ BbacksYoga/src/org/kosta/controller/second/Create_TeacherController.java
@@ -4,13 +4,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.kosta.controller.Controller;
+import org.kosta.model.DAO.TeacherDAO;
 
 public class Create_TeacherController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		String name=request.getParameter("teacherName");
+		String nick=request.getParameter("teacherNick");
+		String profile=request.getParameter("teacherProfile");
+		String imgURL=request.getParameter("teacherImgURL");
+		TeacherDAO.getInstance().AddTeacher(name,nick,profile,imgURL);
+		
+		return "redirect:admin_ver2/admin.jsp";
 	}
 
 }
--- BbacksYoga/src/org/kosta/model/DAO/TeacherDAO.java
+++ BbacksYoga/src/org/kosta/model/DAO/TeacherDAO.java
@@ -1,5 +1,51 @@
 package org.kosta.model.DAO;
 
-public class TeacherDAO {
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.kosta.model.etc.DataSourceManager;
+
+
+
+public class TeacherDAO {
+	private static TeacherDAO instance=new TeacherDAO();
+	private DataSource datasource;
+	private TeacherDAO() {
+		datasource=DataSourceManager.getInstance().getDataSource();
+	}
+	public static TeacherDAO getInstance() {
+		return instance;
+	}
+	public void closeAll(ResultSet rs,PreparedStatement pstmt, Connection con) throws SQLException {
+		if(rs!=null)
+			rs.close();
+		if(pstmt!=null)
+			pstmt.close();
+		if(con!=null)
+			con.close();
+	}
+	public void closeAll(PreparedStatement pstmt, Connection con) throws SQLException {
+		closeAll(null, pstmt, con);
+	}
+	public void AddTeacher(String name, String nick, String profile, String imgURL) throws SQLException {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		try {
+			con=datasource.getConnection();
+			String sql="insert into yoga_teacher(teacherId,teacherName,teacherNick,teacherProfile,imgUrl) values(tchNo_seq.nextval,?,?,?,?)";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setString(2, nick);
+			pstmt.setString(3, profile);
+			pstmt.setString(4, imgURL);
+			pstmt.executeUpdate();			
+		}finally {
+			closeAll(pstmt, con);
+		}		
+	}
 }
