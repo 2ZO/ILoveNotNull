b7beac8634827672fb1ff47bdfada80596c85ee0
kosta <kosta@192.168.0.173>
2018-04-02 09:19:04 +0900

    dd

--- BbacksYoga/src/org/kosta/controller/second/Create_ClassController.java
+++ BbacksYoga/src/org/kosta/controller/second/Create_ClassController.java
@@ -5,12 +5,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.kosta.controller.Controller;
 
+
 public class Create_ClassController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
--- BbacksYoga/src/org/kosta/controller/second/Create_TeacherController.java
+++ BbacksYoga/src/org/kosta/controller/second/Create_TeacherController.java
@@ -4,12 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.kosta.controller.Controller;
+import org.kosta.model.DAO.ProgramDAO;
 
 public class Create_TeacherController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
+		String programName = request.getParameter("programName");
+		String programDetail =request.getParameter("programDetail");
+		ProgramDAO.getInstance().addProgram(programName, programDetail);
 		return null;
 	}
 
