6d5aef744d38a8c915746c73d6db99b2e531a0a2
kosta <kosta@192.168.0.179>
2018-04-03 21:57:18 +0900

    1.0.3 - 주석 추가 - web 주소창 로고 추가 - timetable 음악 제거

--- BbacksYoga/src/org/kosta/controller/third/Read_RegisterController.java
+++ BbacksYoga/src/org/kosta/controller/third/Read_RegisterController.java
@@ -12,14 +12,16 @@ import org.kosta.model.VO.MemberVO;
 import org.kosta.model.VO.RSVO;
 
 public class Read_RegisterController implements Controller {
-
+	//수강내역
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		HttpSession session=request.getSession(false);
 		MemberVO vo=(MemberVO) session.getAttribute("memberVO");//session값 받기
 		if(session==null||vo==null) {
+			//비로그인 시 로그인유도
 			return "/DispatcherServlet?command=page&url=/Member/Login.jsp";
 		}
+		//회원 Id에 저장된 수강내역List 보내기
 		ArrayList<RSVO> list=RSDAO.getInstance().Read_RegisteRStatus(vo.getId());
 		request.setAttribute("registerStatus_list", list);
 		request.setAttribute("url", "/TimeTable/RegisterStatus.jsp");
